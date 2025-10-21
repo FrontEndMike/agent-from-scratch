bd5034c437aae250bedc71f9217d3e1b8fea81a5
Mike Jackson <michael.lenard.jackson@gmail.com>
2025-10-20 20:12:32 -0400

    add lowdb db and conversation history

--- index.ts
+++ index.ts
@@ -1,12 +1,18 @@
 import 'dotenv/config'
 import { runLLM } from './src/llm'
+import { getMessages, addMessages } from './src/memory'
+
 const userMessage = process.argv[2]
 
 if (!userMessage) {
   console.error('Please provide a message')
   process.exit(1)
 }
+await addMessages([{ role: 'user', content: userMessage }])
+const messages = await getMessages()
 
-const response = await runLLM({ userMessage })
+const response = await runLLM({
+  messages,
+})
 
 console.log(response)
